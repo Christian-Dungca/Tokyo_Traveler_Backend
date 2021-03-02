5f3c2b4f8f3899ec38d7779dbd6b4bf32f983e36
Christian-Dungca <cdungca14@gmail.com>
2021-03-02 11:28:56 +0000

    feat: Add user-controller and user-routes files
    Adds files that handle request relating to users. Creates a new user for post request going to /api/users/signup. The SignUp handler adds the new user to the dummy datastructure in memory.

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyParser = require("body-parser");
 
 const HttpError = require("./models/http-error");
 const articleRoutes = require("./routes/article-routes");
+const userRoutes = require('./routes/user-routes');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/articles", articleRoutes);
+app.use("/api/users", userRoutes);
 
 app.use((req, res, next) => {
   const error = new HttpError("Could not find this route", 404);
---
+++ controllers/user-controller.js
@@ -0,0 +1,16 @@
+const { request } = require("express");
+
+const DUMMY_USERS = [
+  { name: "christian", email: "christian@gmail.com", password: "12345" },
+  { name: "jake", email: "jake@gmail.com", password: "12345" },
+  { name: "moon", email: "moon@gmail.com", password: "12345" },
+];
+
+const SignUpUser = (req, res, next) => {
+  const { name, email, password } = req.body;
+  const newUser = { name, email, password };
+
+  res.json({ data: newUser });
+};
+
+exports.SignUpUser = SignUpUser;

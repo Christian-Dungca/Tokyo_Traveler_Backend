e9d1a0f957edc3556ad5e0c62f562020fff27984
Christian-Dungca <cdungca14@gmail.com>
2021-03-15 20:52:11 +0000

    Feat: Create JWT token when signing up a user

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,14 +1,27 @@
+const jwt = require("jsonwebtoken");
 const HttpError = require("../models/http-error");
 const User = require("./../models/user-model");
 
 exports.signup = async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body).catch((err) => {
+    const { name, email, password, passwordConfirm } = req.body;
+
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+      passwordConfirm,
+    }).catch((err) => {
       throw err;
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
     });
 
     res.status(201).json({
       status: "success",
+      token,
       data: {
         user: newUser,
       },
